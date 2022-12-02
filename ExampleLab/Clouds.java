be5e5811abfbbec98cf18162c6aa2214045021eb
Uriah <uriahnaomi.sacdalan@vtsd.com>
2022-12-02 13:46:49 -0500

    d

---
+++ ExampleLab/Clouds.java
@@ -0,0 +1,18 @@
+package ExampleLab;
+import java.awt.*;
+import java.applet.*;
+
+public class Clouds {
+    public static void drawHappyCloud(Graphics g)
+    {
+
+    }
+    public static void drawAngryCloud(Graphics g)
+    {
+
+    }
+    public static void drawSadCloud(Graphics g)
+    {
+
+    }
+}
---
+++ Void1Param/Example.java
@@ -0,0 +1,11 @@
+package Void1Param;
+
+public class Example {
+
+    public static void main(String args[])
+    {
+        Calculator.squareNum(25);
+    }
+
+
+}
